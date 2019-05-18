d4f915bf6d818cdc9abef5f772943a9cbc69f8e8
charlistonrodrigo <charlistonrodrigo@yahoo.com.br>
2019-05-18 20:17:01 -0300

    segundo case

---
+++ transporte/Transporte.java
@@ -0,0 +1,26 @@
+package transporte;
+
+
+public class Transporte {
+
+      public int estoque_jatiboca;
+      public int estoque_pontal;
+      public int ent_est_jatiboca;
+      public int ent_est_pontal;
+      public int sai_est_jatiboca;
+      public int sai_est_pontal;
+
+
+
+
+      public int estoque_inicial_jatiboca( ){
+
+      	//	System.out.println(" INFORME QUAL A QUANTIDADE DE AÇUCAR    : " );
+      	//	ent_est_jatiboca = ler.nextInt();
+      	    //estoque_jatiboca = ent_est_jatiboca;
+             estoque_jatiboca =  ent_est_jatiboca +  estoque_jatiboca;
+      		return estoque_jatiboca  ;
+
+      }
+
+}
