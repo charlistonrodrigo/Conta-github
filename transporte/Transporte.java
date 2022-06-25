047b4d4b45043d8d27b7922e1493a05edd63b3b0
charlistonrodrigo <charlistonrodrigo75@gmail.com>
2022-06-25 15:50:24 -0300

    ALTERAÇÃO DE PACOTES

--- transporte/Transporte.java
+++ transporte/Transporte.java
@@ -14,21 +14,36 @@ public class Transporte {
 
 
       public int estoque_inicial_jatiboca( ){
-
-            estoque_jatiboca =  ent_est_jatiboca +  estoque_jatiboca;
-      		return estoque_jatiboca  ;
-      		}
+          
+          if (ent_est_jatiboca <= 0){
+               System.out.println( " ESTOQUE INSUFICIENTE! DIGITE UM NOVO VALOR!");
+               ent_est_jatiboca = 0;
+          }
+          
+            return estoque_jatiboca =  ent_est_jatiboca +  estoque_jatiboca;
+      		  
+          
+      }		
 
       public int estoque_saida_jatiboca( ){
 
-            estoque_jatiboca =   estoque_jatiboca -  sai_est_jatiboca ;
-      		return estoque_jatiboca  ;
-      		}
+            if ( sai_est_jatiboca > estoque_jatiboca){
+               System.out.println( " ESTOQUE INSUFICIENTE! DIGITE UM NOVO VALOR!");
+               return sai_est_jatiboca = 0;
+             }
+            else if (sai_est_jatiboca < 0){
+                 return sai_est_jatiboca = 0;
+            }
+             return estoque_jatiboca =   estoque_jatiboca -  sai_est_jatiboca ;
+                
+      	    }
+           
+            
 
-      		public int transfere_jatiboca( ){
+      public int transfere_jatiboca( ){
 
-            	estoque_jatiboca = estoque_jatiboca - sai_est_jatiboca;
-    	    	estoque_pontal = estoque_pontal + sai_est_jatiboca ;
+            estoque_jatiboca = estoque_jatiboca - sai_est_jatiboca;
+    	    estoque_pontal = estoque_pontal + sai_est_jatiboca ;
 
       		return estoque_pontal;
       		}
